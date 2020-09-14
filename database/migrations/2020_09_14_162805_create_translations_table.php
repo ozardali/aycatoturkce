c705be1db010ac3d5722d442fbcb86fd0c45bcfe
Göksel Özardalı <gokselozardali@gmail.com>
2020-09-14 19:30:26 +0300

    translations table was created.

---
+++ database/migrations/2020_09_14_162805_create_translations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('ayca');
+            $table->text('turkce')->nullable(true);
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
