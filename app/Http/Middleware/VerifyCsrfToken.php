877da3c1642d19708e3491faecdcee97b04b0392
Göksel Özardalı <gokselozardali@gmail.com>
2020-09-15 21:33:26 +0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'create/translate',
+        'api/v1/create/translate'
     ];
 }
